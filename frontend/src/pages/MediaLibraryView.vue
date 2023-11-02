234fed99259b376e537184a3fd7e9fed4f054545
Hasan Muzak <hasan@apps.com.tr>
2023-11-03 01:35:19 +0300

    Update: Q-Menu CSS & MediaLibraryView changes

--- frontend/src/pages/MediaLibraryView.vue
+++ frontend/src/pages/MediaLibraryView.vue
@@ -3,40 +3,31 @@
     <div class="tw-flex tw-items-center">
       <div class="tw-text-[16px] tw-font-bold">Media Library</div>
       <div class="tw-flex tw-items-center tw-ml-3">
-        <div
-          class="tw-px-2 tw-py-1 tw-cursor-pointer hover:tw-bg-gray-200 tw-rounded-[3px] tw-text-[12px] tw-flex tw-items-center leading-0"
-        >
-          <span v-if="dateRange">
-            {{ dateRange.from }} - {{ dateRange.to }}
-          </span>
-          <span v-else style="line-height: none"> Choose Dates </span>
-          <span class="tw-flex tw-items-center tw-justify-center tw-ml-[4px]">
-            <q-icon size="18px" name="sym_o_calendar_month"></q-icon>
-          </span>
-          <q-popup-proxy transition-show="scale" transition-hide="scale">
-            <q-date v-model="dateRange" range> </q-date>
-          </q-popup-proxy>
-        </div>
-
-        <div
-          class="tw-px-2 tw-py-1 tw-cursor-pointer hover:tw-bg-gray-200 tw-rounded-[3px] tw-text-[12px] tw-flex tw-items-center leading-0 tw-ml-[4px]"
-        >
-          Filter
-          <span class="tw-flex tw-items-center tw-justify-center tw-ml-[4px]">
-            <q-icon size="18px" name="sym_o_filter_list"></q-icon>
-          </span>
-          <q-popup-proxy transition-show="scale" transition-hide="scale">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-            nihil, possimus corporis veniam suscipit rerum, iure consequuntur
-            harum obcaecati dolorum ex eius perferendis illum? Repudiandae illo
-            blanditiis atque fugit saepe.
-          </q-popup-proxy>
-        </div>
+        <CustomDateSelect @change="handleCustomDateSelection" />
+        <CustomDropdown name="Filter" icon="sym_o_filter_list" />
       </div>
     </div>
 
     <div class="tw-mt-3 tw-bg-white tw-rounded-[3px]">
-      <Line />
+      <Line
+        title="Test"
+        unit-title="Test"
+        :categories="['a', 'b', 'c', 'd', 'e', 'f', 'g']"
+        :series="[
+          {
+            name: 'test',
+            data: Array.from({ length: 7 }, () =>
+              Math.floor(Math.random() * 20000)
+            ),
+          },
+          {
+            name: 'test2',
+            data: Array.from({ length: 7 }, () =>
+              Math.floor(Math.random() * 20000)
+            ),
+          },
+        ]"
+      />
     </div>
 
     <div class="tw-text-[16px] tw-font-bold tw-mt-3">All Materials</div>
@@ -56,22 +47,16 @@
           </q-input>
         </div>
         <div class="tw-ml-[8px]">
-          <q-select dense class="tw-w-[160px] modified-icon-input" borderless v-model="selectedChannel" :options="['None', 'Facebook', 'Tiktok', 'Unity']"  label="Dimension" />
+          <q-select
+            dense
+            class="tw-w-[160px] modified-icon-input"
+            borderless
+            v-model="selectedChannel"
+            :options="['None', 'Facebook', 'Tiktok', 'Unity']"
+            label="Dimension"
+          />
         </div>
-        <div
-          class="tw-px-4 tw-py-1 tw-cursor-pointer hover:tw-bg-gray-200 tw-rounded-[3px] tw-text-[12px] tw-flex tw-items-center leading-0 tw-ml-[8px] tw-h-[40px]"
-        >
-          Filter
-          <span class="tw-flex tw-items-center tw-justify-center tw-ml-[4px]">
-            <q-icon size="18px" name="sym_o_filter_list"></q-icon>
-          </span>
-          <q-popup-proxy transition-show="scale" transition-hide="scale">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-            nihil, possimus corporis veniam suscipit rerum, iure consequuntur
-            harum obcaecati dolorum ex eius perferendis illum? Repudiandae illo
-            blanditiis atque fugit saepe.
-          </q-popup-proxy>
-        </div>
+        <CustomDropdown name="Filter" icon="sym_o_filter_list" />
       </div>
       <DataTable />
     </div>
@@ -79,19 +64,30 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import Line from "components/charts/Line.vue";
-import DataTable from "components/media-library-view/DataTable.vue";
+// Import natives, 3rd party modules first...
+import { ref, Ref } from "vue";
 
-const dateRange = ref({
-  from: "2023/10/11",
-  to: "2023/10/25",
-});
+// Components
+import Line from "../components/charts/Line.vue";
+import DataTable from "../components/media-library-view/DataTable.vue";
+import CustomDropdown from "../components/common/CustomDropdown.vue";
+import CustomDateSelect from "../components/common/CustomDateSelect.vue";
 
+// Interfaces
+import { DateRange } from "../components/common/CustomDateSelect";
+// Props
+// Emits
+// Variables
+const dateRange: Ref<DateRange | null> = ref(null);
 const searchText = ref(null);
-const selectedChannel = ref("None")
+const selectedChannel = ref("None");
+// Computed Properties
+// Methods
+function handleCustomDateSelection(value: DateRange): void {
+  dateRange.value = value;
+}
+// Watch
+// Vue Hooks (such as mounted, beforeMounted...)
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
